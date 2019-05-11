ae330431aebb28fdd8f14c78c68c1fa77be6ef57
Reyes Montemayor <sjpete1234@hotmail.com>
2019-05-11 12:41:28 +0900

    setting up validations

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,2 +1,6 @@
 class Repair < ApplicationRecord
+  belongs_to :user
+  belongs_to :order
+  validates :type, presence: true
+  validates :comment, presence: true
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,4 +1,6 @@
 class Shipping < ApplicationRecord
   belongs_to :user
   belongs_to :order
+  validates :receive_courier, exclusion: { in: %w(FedEx UPS USPS DHL) }
+  validates :shipout_courier, exclusion: { in: %w(FedEx UPS USPS DHL) }
 end
