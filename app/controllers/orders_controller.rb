af31322d6cfe40546bc8f930412d8fc6fecd0644
Reyes Montemayor <sjpete1234@hotmail.com>
2019-05-11 23:29:10 +0900

    setting up inital order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,24 @@
 class OrdersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create, :show]
+  skip_before_action :authenticate_user!, only: [:create, :show, :new]
   before_action :order_id_find, only: [:show, :edit, :update, :destroy]
   def index
     @orders = Order.all
   end
 
   def new
-
+    @order = Order.new
+    @product_model = Product.order(:model_no)
   end
 
   def create
-
+    @product_model = Product.order(:model_no)
+    @order = Order.new(order_params)
+    @order.order_status = "Order Created"
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -28,7 +36,7 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
-  def return_params
-    params.require(:order).permit(:first_name, :case_no, :last_name, :address, :address1, :city, :state, :zip, :telephone_no, :email, :product_id)
+  def order_params
+    params.require(:order).permit(:first_name, :case_no, :last_name, :address, :address1, :city, :state, :zip, :telephone_no, :email, :product_id, :serial_number)
   end
 end
